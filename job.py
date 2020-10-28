0eca78fa17afa9ab812dc1d0ee88811dbac767b4
Seth Beauchamp <gcm707@gmail.com>
2020-10-28 10:43:20 -0400

    args

--- job.py
+++ job.py
@@ -8,5 +8,15 @@ def main(runtime):
     # Find the location of the script in relation to the job file
     bgp_tests = os.path.join(os.path.dirname(__file__), 
                              'check_bgp_vrfs.py')
+    import argparse
+    import sys
+    parser = argparse.ArgumentParser(description = "standalone parser")
+    parser.add_argument('--vrf_list', dest = 'vrf_list')
+    args, sys.argv[1:] = parser.parse_known_args(sys.argv[1:])
+    if args.vrf_list:
+        vrf_list = args.vrf_list.split()
+    else:
+        vrf_list = None
+    runtime.tasks.run(testscript=bgp_tests, vrf_list = vrf_list)    
     # Execute the testscript
     runtime.tasks.run(testscript=bgp_tests)
